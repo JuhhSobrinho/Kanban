4a16f160fe5ca453da110bb436d67041b8ef50d0
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-01-26 22:59:51 -0300

    comicação feita com sucesso, houve umas alterações no banco, mas tudo certo

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,4 @@
 // app.js
-const {collection, getDocs, getDoc, doc } = require('firebase/firestore');
-
 
 const { db } = require('./config/firebaseConfig.js');
 
@@ -11,12 +9,39 @@ const port = 3000;
 app.get('/', async (req, res) => {
   try {
     const querySnapshot = await db.collection('id-user').get();
-    const data = querySnapshot.docs.map(doc => doc.data());
-    
-    res.json(data);
-    
-    
+
+    const userData = querySnapshot.docs.map(async (doc) => {
+      const projectName = doc.id; // O ID do documento é o nome do projeto
+
+      const statusData = doc.data().status;
+
+      if (!statusData) {
+        console.log('statusData is undefined');
+        return null; // Retorna nulo para evitar erros adicionais
+      }
+
+      const statusArray = Object.keys(statusData).map((titulo) => ({
+        titulo,
+        tasks: statusData[titulo],
+      }));
+
+      return {
+        projectName,
+        status: statusArray,
+      };
+    });
+
+    // Aguardar todas as promessas serem resolvidas antes de enviar a resposta
+    const userDataResolved = await Promise.all(userData);
+
+    console.log('userDataResolved:', userDataResolved);
+
+    // Filtrar resultados nulos (caso algum tenha sido retornado anteriormente)
+    const filteredUserData = userDataResolved.filter(data => data !== null);
+
+    res.json(filteredUserData);
   } catch (error) {
+    console.error('Erro:', error);
     res.status(500).send(error.message);
   }
 });
