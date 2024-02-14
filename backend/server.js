221ad9ff1c27eec052ea655da82dfb8d6a5d9f9e
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-02-13 23:23:54 -0300

    serve.js arrumando ...

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const { fetchUserData } = require('./models/bdDados.js');
 
-const handler = async (event) => {
+const handler = async (event, res) => {
   try {
     const userData = await fetchUserData();
     res.json(userData);
