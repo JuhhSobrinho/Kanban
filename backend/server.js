88b2d0da2fe95e72f01843ed505588fb4644d1a5
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-02-13 23:19:22 -0300

    serve.js arrumando ...

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const express = require('express');
 const cors = require('cors');
-const handler = express();
 
-handler.use(cors());
-
-handler = async (event) => {
+const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
