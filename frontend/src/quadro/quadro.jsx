7e98489288dae0b66e334f140cb2f5aa296fcf58
JuhhSobrinho <juliano.sobrinhojunior@gmail.com>
2024-02-13 23:48:02 -0300

    visualização de dados

--- frontend/src/quadro/quadro.jsx
+++ frontend/src/quadro/quadro.jsx
@@ -76,7 +76,7 @@ export function Quadro({ quadroId }) {
           }}
         >
           <span className="titulo-post">{`${taskKey}`}</span>
-          <p className="status-post">2 de 4 {`${tasks[index]}`}</p>
+          <p className="status-post">2 de 4 {`${taskKey[index]}`}</p>
         </div>
       ));
     }
